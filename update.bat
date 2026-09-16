d0bd7b03b37071ef59ce9255ca613d43e6854338
ssgoh <iamssgoh@gmail.com>
2026-09-16 17:10:46 +0800

    Add portal, projects, tutorials, and security shield

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 git config user.email "iamssgoh@gmail.com"
-
+git remote set-url origin https://github.com
 git add .
 git commit -m "Add portal, projects, tutorials, and security shield"
 git push origin main
